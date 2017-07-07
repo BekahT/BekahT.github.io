e6403336657e1f9a85fe29a780cc64c9c4d2275f
BekahT <rebekah.treadwell@gmail.com>
2017-07-07 16:42:00 -0400

    js spacing

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 $(document).ready(function() {
+
+  
   $('.skill-category').hide();
 
   $('.skill-button').on('click', function() {
     $(this).next().slideToggle(400); });
 
   $('#skillTable').DataTable();
+
+
 });

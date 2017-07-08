649a8e03e3820b24116401d6595d0c149781f99c
BekahT <rebekah.treadwell@gmail.com>
2017-07-08 16:32:00 -0400

    skill table improvements

--- main.js
+++ main.js
@@ -9,6 +9,17 @@ $(document).ready(function() {
 
     // skill table features
   $('#skillTable').DataTable( {
-    responsive: true
+    responsive: true,
+    order: [2, 'desc'], //default sort on completed date, descending
+    lengthMenu: [ [5, 10, 25, 50, -1], [5, 10, 25, 50, "All"] ], //table length options, -1 shows all
+    pageLength: 10, //set default display to 10 rows
+    pagingType: "full_numbers", //'First', 'Previous', 'Next' and 'Last' buttons, plus page numbers
+    //change search box title and pagination buttons words to signs
+    language: {
+        search: "Filter Courses:",
+        paginate: {first: '«', previous: '‹', next: '›', last: '»'},
+        aria: {paginate: {first: 'First', previous: 'Previous', next: 'Next', last: 'Last'}}
+    },
   });
+
 });
